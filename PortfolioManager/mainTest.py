2980985183ea70058d655695b437cbd73d190fb0
afunes <afunes@192.168.2.12>
2017-05-03 22:31:07 -0500

    36

--- PortfolioManager/mainTest.py
+++ PortfolioManager/mainTest.py
@@ -3,10 +3,3 @@ Created on Mar 8, 2017
 
 @author: afunes
 '''
-from gui.movementEditor2 import MovementEditor
-
-def main():
-    app = MovementEditor()
-    
-if __name__== '__main__':
-    main()
