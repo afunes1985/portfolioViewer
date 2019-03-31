bf1171d4dad246d6f0f6fb459c884d5f26975beb
afunes <afunes@SISTEMAS044.ar.lumina.net>
2019-03-31 11:42:19 -0600

    fix dump exporter

--- PortfolioManager/core/dumpexporter.py
+++ PortfolioManager/core/dumpexporter.py
@@ -14,7 +14,7 @@ class DumpExporter():
         fileName = time.strftime("%Y%m%d") + "_" + "dump_"+ dbName 
         BACKUP_PATH = 'C:\\Users\\afunes\\iCloudDrive\\PortfolioViewer\\dumps\\'
          
-        dumpcmd = """"C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysqldump.exe\"""" + " -h " + dbHost + " -u " + dbUser + " -p" + dbUserPass + " " + dbName +  " > " + BACKUP_PATH + fileName + ".sql"
+        dumpcmd = """"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe\"""" + " -h " + dbHost + " -u " + dbUser + " -p" + dbUserPass + " " + dbName +  " > " + BACKUP_PATH + fileName + ".sql"
         os.system(dumpcmd)
         print(dumpcmd)
         print ("Backup script completed " + fileName)
@@ -22,4 +22,4 @@ class DumpExporter():
     @staticmethod  
     def exportAllDump(self):
         DumpExporter.exportDump('localhost', 'root', 'root', 'portfolio')
-        DumpExporter.exportDump('localhost', 'root', 'root', 'fundamenalanalytics')
+        DumpExporter.exportDump('localhost', 'root', 'root', 'fundamentalanalytics')
